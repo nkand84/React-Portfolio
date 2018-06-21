36ef8f06a1c3090534f6c629efaba73dfa5c63d9
nkand84 <nityabhagi@gmail.com>
2018-06-20 22:55:52 -0700

    added navbar component and background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-class App extends Component {
-  render() {
-    return(
-   <div> Ready? </div>
-    );
-  }
-}
+
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+     
+        
+     
+    </div>
+  </Router>
+);
 
 export default App;
