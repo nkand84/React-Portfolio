fd78596e80b1cca6792017f7dcd47d1d44498f04
nkand84 <nityabhagi@gmail.com>
2018-06-20 23:07:03 -0700

    navbar with css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import "./Navbar.css";
 
 const Navbar = () =>
 
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg">
         <Link className="navbar-brand" to="/">Nitya Kandukuri</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
